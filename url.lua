2fcd06d78e29b7bfce9e033b0d3036d7ae957246
B1nk <B1nk@users.noreply.github.com>
2014-11-25 08:59:52 +0800

    1.0

---
+++ url.lua
@@ -0,0 +1,16 @@
+function get(url)
+	local handle = io.popen("curl -q -k -s -m 1 "..url)
+	local result = handle:read ("*a")
+	handle:close()
+	return result
+	
+end
+
+
+function post(url, fields)
+	local handle = io.popen("curl -q -k -s -m 1 "..url.." -d '"..fields.."'")
+	local result = handle:read("*a")
+	handle:close()
+	return result
+	
+end
