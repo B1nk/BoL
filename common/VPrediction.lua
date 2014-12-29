6d27c3a9b4aa1ae8f1beac21121464338535349d
B1nk <B1nk@users.noreply.github.com>
2014-12-29 09:07:01 +0800

    Update VPrediction.lua

--- common/VPrediction.lua
+++ common/VPrediction.lua
@@ -2,13 +2,13 @@ local version = "1.11"
 local TESTVERSION = false
 local AUTOUPDATE = false
 local UPDATE_HOST = "raw.github.com"
-local UPDATE_PATH = "/Ralphlol/BoLGit/master/VPrediction.lua".."?rand="..math.random(1,10000)
+local UPDATE_PATH = "/B1nk/BoL/master/common/VPrediction.lua".."?rand="..math.random(1,10000)
 local UPDATE_FILE_PATH = LIB_PATH.."vPrediction.lua"
 local UPDATE_URL = "https://"..UPDATE_HOST..UPDATE_PATH
 
 local function AutoupdaterMsg(msg) print("<font color=\"#6699ff\"><b>VPrediction TEMP FIX:</b></font> <font color=\"#FFFFFF\">"..msg..".</font>") end
 if AUTOUPDATE then
-	local ServerData = GetWebResult(UPDATE_HOST, "/Ralphlol/BoLGit/master/VPrediction.version")
+	local ServerData = GetWebResult(UPDATE_HOST, "/B1nk/BoL/master/version/VPrediction.version")
 	if ServerData then
 		ServerVersion = type(tonumber(ServerData)) == "number" and tonumber(ServerData) or nil
 		if ServerVersion then
